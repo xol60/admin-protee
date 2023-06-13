3768212c2f9db299d1c5563da837164db5350d96
kaballl <76085266+kaballl@users.noreply.github.com>
2023-06-13 12:50:22 +0700

    update location list

--- src/components/Location/Item.tsx
+++ src/components/Location/Item.tsx
@@ -1,46 +1,57 @@
 import { EditOutlined } from '@ant-design/icons'
-import { Avatar, Button, Checkbox, Form, Input, List, Modal, Select, Skeleton } from 'antd'
-import React, { useState, useCallback, useRef, useEffect  } from 'react'
+import { Avatar, Button, Form, List, Modal, Select, Skeleton } from 'antd'
+import React from 'react'
 import api from '../../api/axiosClient'
-import { getGlobalState, setGlobalState } from '../../state'
-import {GoogleMapsProvider,useGoogleMap} from '@ubilabs/google-maps-react-hooks';
-import { Marker } from '@react-google-maps/api'
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { useGoogleMap } from '@ubilabs/google-maps-react-hooks';
+import { toast } from 'react-toastify';
+import { User } from '../../module/user.dto';
+
+import { useNavigate } from 'react-router-dom';
 
 const Item = (i: any) => {
-  
-  
+  const [user,setUser]=React.useState<User>();
+ 
+  try {
+    const res = api.users.detail(i.i.createdBy)
+    Promise.all([res]).then(values => {
+        setUser(values[0]);
+        
+    });
+    }
+    catch (err) {
+      console.log(err);
+    }
+ 
   const [form1] = Form.useForm();
-  
-  const[lat,setLat]=useState(Number(i.i.lat))
-  
-  const[lng,setLng]=useState(Number(i.i.long))
-  const map=useGoogleMap()
-  const marker=new google.maps.Marker({map})
-  marker.setPosition({lng,lat})
-  
+  const lat = Number(i.i.lat)
+  const lng = Number(i.i.long)
+  const map = useGoogleMap()
+  const marker = new google.maps.Marker({ map })
+  marker.setPosition({ lng, lat })
   marker.setTitle(i.i.name)
-  const [color,setColor]=useState("blue");
-  const [confirmedStatus, setConfirmedStatus] = useState(i.i.status)
-  if(confirmedStatus=='personal'){
+  const [color, setColor] = React.useState("blue");
+  const [confirmedStatus, setConfirmedStatus] = React.useState('')
+  const [status, setStatus] = React.useState('')
+  React.useEffect(() => {
+    setConfirmedStatus(i.status);
+    setStatus(i.status)
+  }, [i.status])
+  if (confirmedStatus === 'personal') {
     setConfirmedStatus('Personal')
     setColor("black")
   }
-  if(confirmedStatus=='published'){
+  if (confirmedStatus === 'published') {
     setConfirmedStatus('Published')
     setColor("red")
   }
-  if(confirmedStatus=='waiting_publish'){
+  if (confirmedStatus === 'waiting_publish') {
     setConfirmedStatus('WaitingPublish')
     setColor("purple")
   }
-  if(confirmedStatus=='hidden'){
+  if (confirmedStatus === 'hidden') {
     setConfirmedStatus('Hidden')
     setColor("blue")
-    
   }
-
   const svgMarker = {
     path: "M-1.547 12l6.563-6.609-1.406-1.406-5.156 5.203-2.063-2.109-1.406 1.406zM0 0q2.906 0 4.945 2.039t2.039 4.945q0 1.453-0.727 3.328t-1.758 3.516-2.039 3.070-1.711 2.273l-0.75 0.797q-0.281-0.328-0.75-0.867t-1.688-2.156-2.133-3.141-1.664-3.445-0.75-3.375q0-2.906 2.039-4.945t4.945-2.039z",
     fillColor: color,
@@ -51,107 +62,84 @@ const Item = (i: any) => {
     anchor: new google.maps.Point(0, 20),
   };
   marker.setIcon(svgMarker)
-  
-  const [status, setStatus] = useState(i.i.status)
-  
- 
-  
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  
- 
+  const [isModalOpen, setIsModalOpen] = React.useState(false);
+
+
   const showModal = () => {
     setIsModalOpen(true);
 
   };
-
   const handleOk = () => {
     setIsModalOpen(false);
-    if(status=='Personal'){
-        
-      setColor("black")
-    }
-    if(status=='Published'){
-      
-      setColor("red")
-    }
-    if(status=='WaitingPublish'){
-    
-      setColor("purple")
-    }
-    if(status=='Hidden'){
-      
-      setColor("blue")
-      
-    }
-
-    setConfirmedStatus(status)
-    
-   
- 
-    try {
-      
-      
-      const res = api.location.changedetail({
-          "id": i.i.id,
-          "status":status
-      })
-      Promise.all([res]).then(values => {
-          if (values[0]) {
-
-            toast.success("Update successfully",{
-              position:toast.POSITION.TOP_CENTER,
-              theme:"colored"
-            })
-
-          }
-      });
-  }
-  catch (err) {
-    toast.error("Update fail",{
-      position:toast.POSITION.TOP_CENTER,
-      theme:"colored"
+    const res = api.location.changedetail({
+      "id": i.i.id,
+      "status": status
+    })
+    Promise.all([res]).then(values => {
+      if (values[0]) {
+        toast.success("Update successfully", {
+          position: toast.POSITION.TOP_CENTER,
+          theme: "colored"
+        })
+        if (status === 'Personal') {
+          setColor("black")
+        }
+        if (status === 'Published') {
+          setColor("red")
+        }
+        if (status === 'WaitingPublish') {
+          setColor("purple")
+        }
+        if (status === 'Hidden') {
+          setColor("blue")
+        }
+        setConfirmedStatus(status)
+      }
+    }).catch(error => {
+      if (error.response.status === 400) {
+        toast.error("Update fail", {
+          position: toast.POSITION.TOP_CENTER,
+          theme: "colored"
+        })
+      }
     })
   }
-
-    
-
-
-  }
-
   const handleCancel = () => {
     setIsModalOpen(false);
     form1.resetFields()
-
-
-
   }
-  const click=()=>{
-    
-    map?.panTo({lat,lng})
-    
-    
+  const Centerclick = () => {
+    map?.panTo({ lat, lng })
   }
+  const navigate=useNavigate()
+  const Userclick=()=>{
+    if(user){
+      navigate(`/user/${user.id}`)
+    }
+  }
+  
   return (
     <List.Item
       actions={[<Button onClick={showModal}><EditOutlined /></Button>]}
     >
-
       <Skeleton avatar title={false} loading={i.i.loading} active>
         <List.Item.Meta
-
-        
-
-          title={<a onClick={click}>{i.i.name}</a>}
+          avatar={<Avatar src={i.i.icon} />}
+          title={<a onClick={Centerclick}>{i.i.name}</a>}
           description={i.i.description}
-
         />
         <List.Item.Meta
-          title={confirmedStatus}></List.Item.Meta>
-
-
-
-
-
+          description={<p >{confirmedStatus}</p>}
+          title="Status"
+          ></List.Item.Meta>
+          
+        <List.Item.Meta
+          avatar={<Avatar onClick={Userclick} src={user?.avt||"https://i.ibb.co/2MQCBJD/user-icon-vector-260nw-393536320.webp"} />}
+          title="Creator"
+          description={<a onClick={Userclick}>{user?.name||"Admin"}</a>}
+         
+        />
+        
       </Skeleton>
       <Modal title="Update Location" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
         <Form
@@ -159,37 +147,27 @@ const Item = (i: any) => {
           labelCol={{ span: 8 }}
           wrapperCol={{ span: 16 }}
           style={{ maxWidth: 600 }}
-          initialValues={{ remember: true }}
+          initialValues={{
+            status: confirmedStatus,
+            remember: true
+          }}
           form={form1}
-
           autoComplete="off"
         >
           <Form.Item label="Status"
-      name="status">
+            name="status">
             <Select id='123' onChange={(value) => {
               setStatus(value)
-            }} defaultValue={confirmedStatus}
+            }}
               options={[
                 { value: 'Personal', label: 'Personal' },
                 { value: 'Published', label: 'Published' },
                 { value: 'WaitingPublish', label: 'WaitingPublish' },
                 { value: 'Hidden', label: 'Hidden', },]}>
-
-
             </Select>
-
           </Form.Item>
         </Form>
-
-
-
-
-
-
       </Modal>
-      <ToastContainer />
-
-
     </List.Item>
   )
 }
--- src/components/Location/ListItem.tsx
+++ src/components/Location/ListItem.tsx
@@ -2,191 +2,201 @@ import { Box, Flex, SkeletonCircle } from '@chakra-ui/react'
 
 
 import Item from './Item';
-
-import { Avatar, Button, Checkbox, Form, Input, InputNumber, List, Modal, Select, Skeleton, Tooltip } from 'antd'
-import React, { useState, useCallback, useRef, useEffect  } from 'react'
-import {GoogleMapsProvider, useGoogleMap } from '@ubilabs/google-maps-react-hooks';
-import { ConsoleSqlOutlined, PlusOutlined } from '@ant-design/icons';
+import { useSearchParams } from "react-router-dom"
+import { Button, Form, Input, List, Modal, Tooltip, Dropdown, Space } from 'antd'
+import type { MenuProps } from 'antd';
+import { DownOutlined } from '@ant-design/icons';
+import React, { useState, } from 'react'
+import { useGoogleMap } from '@ubilabs/google-maps-react-hooks';
+import { HomeOutlined, PlusOutlined, SearchOutlined, UndoOutlined } from '@ant-design/icons';
 import api from '../../api/axiosClient';
 import { DangerousLocation } from '../../module/location.dto';
 import PlacesAutocomplete from 'react-places-autocomplete';
 import {
   geocodeByAddress,
-
   getLatLng,
 } from 'react-places-autocomplete';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import { color } from 'framer-motion';
+import { toast } from 'react-toastify';
+import { useNavigate } from 'react-router-dom';
+import { QueryLocationDto } from '../../module/location.dto'
+import SearchModal from '../Modal/SearchModal'
 
+const sortValues: MenuProps['items'] = [
+  {
+    label: 'Name',
+    key: 'name',
+  },
+  {
+    label: 'Status',
+    key: 'status',
+  },
+  {
+    label: 'Description',
+    key: 'description',
+  },
+];
 
+const statusValues: MenuProps['items'] = [
+  {
+    label: 'Personal',
+    key: 'personal',
+  },
+  {
+    label: 'Waitting Publish',
+    key: 'waiting_publish',
+  },
+  {
+    label: 'Published',
+    key: 'published',
+  },
+  {
+    label: 'Hidden',
+    key: 'hidden',
+  },
+];
 
-<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBMrRrm1EiDwptZuK3bfhrJyF2x9qIcn0A&libraries=places"></script>
+<script src={"https://maps.googleapis.com/maps/api/js?key=" + process.env.REACT_APP_GOOGLEMAP_KEY + "&libraries=places"}></script>
 const ListItem = () => {
- 
-  
   const [locations, setLocations] = React.useState<DangerousLocation[]>([])
+  const navigate = useNavigate()
+  const [queryParameters] = useSearchParams()
+  const [query, setQuery] = React.useState<QueryLocationDto>({ filter: queryParameters.get("filter") + '', sortField: queryParameters.get("sortField") + '', status: queryParameters.get("status") + '' })
+  const [isModalOpen, setIsModalOpen] = React.useState<boolean>(false);
   React.useEffect(() => {
     try {
-      const res1 = api.location.list({ filter: '' });
-      
+      const res1 = api.location.list(query);
+
       Promise.all([res1]).then(values => {
-        
+
         setLocations(values[0]);
       });
     }
     catch (err) {
       console.error(err)
     }
-  }, [])
-  const map=useGoogleMap()
- 
-
- 
+  }, [query])
+  React.useEffect(() => {
+    setQuery({ filter: queryParameters.get("filter") + '', sortField: queryParameters.get("sortField") + '', status: queryParameters.get("status") + '' })
+  }, [queryParameters]);
+  const  map  = useGoogleMap()
   
-    map?.addListener("click", (mapsMouseEvent:any) => {
-    
-   
+
+
+
+
+  map?.addListener("click", (mapsMouseEvent: any) => {
     setLat(mapsMouseEvent.latLng.toJSON().lat)
     setLng(mapsMouseEvent.latLng.toJSON().lng)
-    
-    
-    
-  
-    
     setIsModalOpen(true)
     form.resetFields()
-    
   });
-
-
- const [lat,setLat]=useState(0);
- const[lng,setLng]=useState(0);
-  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [lat, setLat] = useState(0);
+  const [lng, setLng] = useState(0);
+  const [isOpen, setIsOpen] = useState(false);
   const [form] = Form.useForm();
- 
-
-
-
   const handleCancel = () => {
     setIsModalOpen(false);
-   
+
   }
   const onFinish = (values: any) => {
-    const pro=api.location.create({
-    name: values.name,
-    description: values.address,
-    long: lng,
-    lat: lat
-
-
+    setIsModalOpen(false)
+    const pro = api.location.create({
+      name: values.name,
+      description: values.address,
+      long: lng,
+      lat: lat
     })
     Promise.all([pro]).then(values => {
-      if (values[0]) {
-        setLocations([...locations,values[0]])
-       
-        setIsModalOpen(false)
-        toast.success("Add new dangerous location successfully",{
-          position:toast.POSITION.TOP_CENTER,
-          theme:"colored"
+     
+      setLocations([...locations, values[0]])
+
+     
+      toast.success("Add new dangerous location successfully", {
+        position: toast.POSITION.TOP_CENTER,
+        theme: "colored"
+      })
+    }).catch(error => {
+      if (error.response.status === 400) {
+        toast.error("Add new dangerous location fail.Please check your input address", {
+          position: toast.POSITION.TOP_CENTER,
+          theme: "colored"
         })
-        
       }
-      else {
-        toast.error("Add new dangerous location fail.Please check your input address",{
-          position:toast.POSITION.TOP_CENTER,
-          theme:"colored"
-        })
-      }
-      
-    });
-    
-      
-    
-   
+    })
+
+
+
+
 
   };
   const [form1] = Form.useForm();
-  
+
   const onFinishFailed = (errorInfo: any) => {
+
+  };
+  const onSortClick: MenuProps['onClick'] = ({ key }) => {
+    navigate(`/locations?&filter=${query.filter + ''}&sortField=${key}&status=${query.status}`)
     
   };
-
+  const onStatusClick: MenuProps['onClick'] = ({ key }) => {
+   
+    navigate(`/locations?&filter=${query.filter + ''}&sortField=${query.sortField}&status=${key}`)
+    
+  };
   const [isModalOpen1, setIsModalOpen1] = useState(false);
   const handleCancel1 = () => {
-    
+
     setIsModalOpen1(false);
 
   }
   const onFinishFailed1 = (errorInfo: any) => {
-    
+
   };
   const onFinish1 = (values: any) => {
+    setIsModalOpen1(false)
     geocodeByAddress(values.address)
-    .then((results:any) => getLatLng(results[0]))
-    .then((latLng:any) => {
-    const pro=api.location.create({
-      name: values.name,
-      description: values.address,
-      long: latLng.lng,
-      lat: latLng.lat
-  
-  
-      })
-      Promise.all([pro]).then(values => {
-        if (values[0]) {
-          setLocations([...locations,values[0]])
-          setIsModalOpen1(false)
-          toast.success("Add new dangerous location successfully",{
-            position:toast.POSITION.TOP_CENTER,
-            theme:"colored"
-          })
-          
-        }
-        else {
-         
-        }
-        
-      });
-     })
-     .catch(error => {
-          setIsModalOpen1(false)
-          toast.error("Add new dangerous location fail.Please check your input address",{
-            position:toast.POSITION.TOP_CENTER,
-            theme:"colored"
-          })
-        });
-    
-    
+      .then((results: any) => getLatLng(results[0]))
+      .then((latLng: any) => {
+        const pro = api.location.create({
+          name: values.name,
+          description: values.address,
+          long: latLng.lng,
+          lat: latLng.lat
+        })
+        Promise.all([pro]).then(values => {
+          if (values[0]) {
+            setLocations([...locations, values[0]])
+            
+            toast.success("Add new dangerous location successfully", {
+              position: toast.POSITION.TOP_CENTER,
+              theme: "colored"
+            })
 
-   
-   
-   
+          }
+        });
+      })
+      .catch(error => {
+        if (error.response.status === 400) {
+          setIsModalOpen1(false)
+          toast.error("Add new dangerous location fail.Please check your input address", {
+            position: toast.POSITION.TOP_CENTER,
+            theme: "colored"
+          })
+        }
+      });
 
   };
-  const [address,setAddress]=useState("");
- const handleChange = (address:any) => {
+  const [address, setAddress] = useState("");
+  const handleChange = (address: any) => {
     setAddress(address)
   };
- 
-  const handleSelect = (address:any) => {
-   
-    form1.setFieldsValue({address:address})
-    
-    
-  
+  const handleSelect = (address: any) => {
+    form1.setFieldsValue({ address: address })
   };
-  const showModal1=()=>{
+  const showModal1 = () => {
     setIsModalOpen1(true)
     form1.resetFields()
   }
-  
-  
-  
-
-
-  
-  
 
   if (!true)
     return (
@@ -226,7 +236,7 @@ const ListItem = () => {
     );
 
   return (
-    
+
     <Flex
       direction={"column"}
       bg={"whiteAlpha.900"}
@@ -239,157 +249,155 @@ const ListItem = () => {
       overflow="hidden"
       px={2}
     >
-      
+
       <Flex bg={'white'} overflowY={"scroll"} mt={60} direction={"column"}>
-      
-      <Tooltip title="Add location">
-        <Button onClick={showModal1} type="primary" shape="circle" icon={<PlusOutlined />} />
-      </Tooltip>
-      
-      <Modal title="Add Location" open={isModalOpen}  onCancel={handleCancel} footer={[
-          
-          
+
+        <Tooltip  >
+          <Button style={{ margin: '10px' }} title='Come back Home Page' onClick={() => { navigate('/homepage') }} type="primary" shape="circle" size="large" icon={<HomeOutlined />}></Button>
+          <Dropdown menu={{ items: sortValues, onClick: onSortClick }}>
+            <a onClick={(e) => e.preventDefault()}>
+              <Button style={{ margin: '10px' }} type="primary" size="large" icon={<DownOutlined />}>Sort By</Button>
+            </a>
+          </Dropdown>
+          <Dropdown menu={{ items: statusValues, onClick: onStatusClick }}>
+            <a onClick={(e) => e.preventDefault()}>
+              <Button style={{ margin: '10px' }} type="primary" size="large" icon={<DownOutlined />}>Status</Button>
+            </a>
+          </Dropdown>
+          <Button style={{ margin: '10px' }} title='Add a new location' onClick={showModal1} type="primary" size="large" shape="circle" icon={<PlusOutlined />}></Button>
+          <Button style={{ margin: '10px' }} title='Search name of location' onClick={() => { setIsOpen(true) }} type="primary" shape="circle" size="large" icon={<SearchOutlined />}></Button>
+          <Button style={{ margin: '10px' }} title='Refresh condition' onClick={() => { navigate(`/locations?&filter=&sortField=&status=`) }} shape="circle" type="primary" size="large" icon={<UndoOutlined />}></Button>
+        </Tooltip>
+
+        <Modal title="Add Location" open={isModalOpen} onCancel={handleCancel} footer={[
+
+
         ]}>
-        <Form
-        form={form}
-        
-          name="basic"
-          labelCol={{ span: 8 }}
-          wrapperCol={{ span: 16 }}
-          style={{ maxWidth: 600 }}
-          initialValues={{ remember: true }}
-          onFinish={onFinish}
-    onFinishFailed={onFinishFailed}
+          <Form
+            form={form}
 
-          autoComplete="off"
-        >
-          
-    <Form.Item
-      label="Name"
-      name="name"
-      rules={[{ required: true, message: 'Please input the name of location!' }]}
-    >
-      <Input  placeholder='Trường Đại học Khoa học Tự nhiên ' />
-    </Form.Item>
-    <Form.Item
-      label="Address"
-      name="address"
-      rules={[{ required: true, message: 'Please input the address of location!' }]}
-    >
-      <Input  placeholder='227 Đ. Nguyễn Văn Cừ, Phường 4, Quận 5, Thành phố Hồ Chí Minh' />
-    </Form.Item>
-        
-    
-     
-            
-      
-      <Form.Item wrapperCol={{ offset: 10, span: 16 }}>
-        
+            name="basic"
+            labelCol={{ span: 8 }}
+            wrapperCol={{ span: 16 }}
+            style={{ maxWidth: 600 }}
+            initialValues={{ remember: true }}
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
 
-      <Button type="primary" htmlType="submit">
-        Submit
-      </Button>
-    </Form.Item>
-        </Form>
+            autoComplete="off"
+          >
+
+            <Form.Item
+              label="Name"
+              name="name"
+              rules={[{ required: true, message: 'Please input the name of location!' }]}
+            >
+              <Input placeholder='Trường Đại học Khoa học Tự nhiên ' />
+            </Form.Item>
+            <Form.Item
+              label="Address"
+              name="address"
+              rules={[{ required: true, message: 'Please input the address of location!' }]}
+            >
+              <Input placeholder='227 Đ. Nguyễn Văn Cừ, Phường 4, Quận 5, Thành phố Hồ Chí Minh' />
+            </Form.Item>
 
 
 
 
 
+            <Form.Item wrapperCol={{ offset: 10, span: 16 }}>
+              <Button type="primary" htmlType="submit">
+                Submit
+              </Button>
+            </Form.Item>
+          </Form>
 
-      </Modal>
-           
-            
-      <Modal title="Add Location" open={isModalOpen1}  onCancel={handleCancel1} footer={[
-          
-          
+        </Modal>
+
+
+        <Modal title="Add Location" open={isModalOpen1} onCancel={handleCancel1} footer={[
+
+
         ]}>
-        <Form
-        form={form1}
-        
-          name="basic"
-          labelCol={{ span: 8 }}
-          wrapperCol={{ span: 16 }}
-          style={{ maxWidth: 600 }}
-          initialValues={{ remember: true }}
-          onFinish={onFinish1}
-    onFinishFailed={onFinishFailed1}
+          <Form
+            form={form1}
 
-          autoComplete="off"
-        >
-          
-          <Form.Item
-      label="Name"
-      name="name"
-      rules={[{ required: true, message: 'Please input the name of location!' }]}
-    >
-      <Input placeholder='Trường Đại học Khoa học Tự nhiên ' />
-    </Form.Item>     
-    <Form.Item
-      label="Address"
-     name="address"
-      rules={[{ required: true, message: 'Please input the address of location!' }]}
-    >
-      <PlacesAutocomplete
-        value={address}
-        onChange={handleChange}
-        onSelect={handleSelect}
-      >
-        {({ getInputProps, suggestions, getSuggestionItemProps, loading }:any) => (
-          <div>
-            <Input width='100%'
-              {...getInputProps({
-                placeholder: 'Search Places ...',
-                className: 'location-search-input',
-              })}
-            />
-            <div className="autocomplete-dropdown-container">
-              {loading && <div>Loading...</div>}
-              {suggestions.map((suggestion:any) => {
-                const className = suggestion.active
-                  ? 'suggestion-item--active'
-                  : 'suggestion-item';
-                // inline style for demonstration purpose
-                const style = suggestion.active
-                  ? { backgroundColor: '#689ee4', cursor: 'pointer' }
-                  : { backgroundColor: '#ffffff', cursor: 'pointer' };
-                return (
-                  <div
-                    {...getSuggestionItemProps(suggestion, {
-                      className,
-                      style,
-                    })}
-                  >
-                    <span>{suggestion.description}</span>
+            name="basic"
+            labelCol={{ span: 8 }}
+            wrapperCol={{ span: 16 }}
+            style={{ maxWidth: 600 }}
+            initialValues={{ remember: true }}
+            onFinish={onFinish1}
+            onFinishFailed={onFinishFailed1}
+
+            autoComplete="off"
+          >
+
+            <Form.Item
+              label="Name"
+              name="name"
+              rules={[{ required: true, message: 'Please input the name of location!' }]}
+            >
+              <Input placeholder='Trường Đại học Khoa học Tự nhiên ' />
+            </Form.Item>
+            <Form.Item
+              label="Address"
+              name="address"
+              rules={[{ required: true, message: 'Please input the address of location!' }]}
+            >
+              <PlacesAutocomplete
+                value={address}
+                onChange={handleChange}
+                onSelect={handleSelect}
+              >
+                {({ getInputProps, suggestions, getSuggestionItemProps, loading }: any) => (
+                  <div>
+                    <Input width='100%'
+                      {...getInputProps({
+                        placeholder: 'Search Places ...',
+                        className: 'location-search-input',
+                      })}
+                    />
+                    <div className="autocomplete-dropdown-container">
+                      {loading && <div>Loading...</div>}
+                      {suggestions.map((suggestion: any) => {
+                        const className = suggestion.active
+                          ? 'suggestion-item--active'
+                          : 'suggestion-item';
+                        // inline style for demonstration purpose
+                        const style = suggestion.active
+                          ? { backgroundColor: '#689ee4', cursor: 'pointer' }
+                          : { backgroundColor: '#ffffff', cursor: 'pointer' };
+                        return (
+                          <div
+                            {...getSuggestionItemProps(suggestion, {
+                              className,
+                              style,
+                            })}
+                          >
+                            <span>{suggestion.description}</span>
+                          </div>
+                        );
+                      })}
+                    </div>
                   </div>
-                );
-              })}
-            </div>
-          </div>
-        )}
-      </PlacesAutocomplete>
-      
-    </Form.Item>
-        
-         
-     
-            
-      
-      <Form.Item wrapperCol={{ offset: 10, span: 16 }}>
-        
+                )}
+              </PlacesAutocomplete>
 
-      <Button type="primary" htmlType="submit">
-        Submit
-      </Button>
-    </Form.Item>
-        </Form>
+            </Form.Item>
 
 
 
 
 
-
-      </Modal>
+            <Form.Item wrapperCol={{ offset: 10, span: 16 }}>
+              <Button type="primary" htmlType="submit">
+                Submit
+              </Button>
+            </Form.Item>
+          </Form>
+        </Modal>
         <List
           className="demo-loadmore-list"
 
@@ -397,16 +405,12 @@ const ListItem = () => {
 
           dataSource={locations}
           renderItem={(item) => (
-            <Item i={item}  ></Item>
+            <Item i={item} status={item.status} ></Item>
 
           )}
         />
-         
-
-
-
-         <ToastContainer />
       </Flex>
+      <SearchModal isOpen={isOpen} setIsOpen={setIsOpen} isLocation={true} />
     </Flex>
   )
 }
