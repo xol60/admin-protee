9bd4c2894cfeb17234be26087ae3eb36203bcc06
xol <xold59@gmail.com>
2023-06-17 02:09:42 +0700

    fix deplicate error

--- src/api/axiosClient.ts
+++ src/api/axiosClient.ts
@@ -18,57 +18,59 @@ const WithAxios: React.FC<any> = ({ children }) => {
         setLoading(true);
         return config;
     });
-    axios.interceptors.response.use(
-        (res) => {
-            setLoading(false);
-            return res;
-        },
-        (error: AxiosError) => {
-            setLoading(false);
-            const { status } = error.response!;
-            switch (status) {
+    React.useEffect(() => {
+        axios.interceptors.response.use(
+            (res) => {
+                setLoading(false);
+                return res;
+            },
+            (error: AxiosError) => {
+                setLoading(false);
+                const { status } = error.response!;
+                switch (status) {
 
-                case 401:
-                    console.error('unauthorised');
-                    const cookies = new Cookies();
-                    cookies.remove('jwt_authentication')
-                    toast.error('Please sign in first!', {
-                        position: toast.POSITION.TOP_CENTER,
-                        theme: "colored"
-                    });
-                    break;
+                    case 401:
+                        console.error('unauthorised');
+                        const cookies = new Cookies();
+                        cookies.remove('jwt_authentication')
+                        toast.error('Please sign in first!', {
+                            position: toast.POSITION.TOP_CENTER,
+                            theme: "colored"
+                        });
+                        break;
 
-                case 403:
-                    toast.error('Forbidden! You do not have permission to access!', {
-                        position: toast.POSITION.TOP_CENTER,
-                        theme: "colored"
-                    });
-                    break;
-                case 404:
-                    console.error('/not-found');
-                    toast.error('Oops! Something went error. Please contact your administrator!', {
-                        position: toast.POSITION.TOP_CENTER,
-                        theme: "colored"
-                    });
-                    break;
+                    case 403:
+                        toast.error('Forbidden! You do not have permission to access!', {
+                            position: toast.POSITION.TOP_CENTER,
+                            theme: "colored"
+                        });
+                        break;
+                    case 404:
+                        console.error('/not-found');
+                        toast.error('Oops! Something went error. Please contact your administrator!', {
+                            position: toast.POSITION.TOP_CENTER,
+                            theme: "colored"
+                        });
+                        break;
 
-                case 500:
-                    console.error('/server-error');
-                    toast.error('Server is having trouble! Please try again later!', {
-                        position: toast.POSITION.TOP_CENTER,
-                        theme: "colored"
-                    });
-                    break;
-                default:
-                    toast.error('Unknown error. Please check and try again!', {
-                        position: toast.POSITION.TOP_CENTER,
-                        theme: "colored"
-                    });
-                    break;
+                    case 500:
+                        console.error('/server-error');
+                        toast.error('Server is having trouble! Please try again later!', {
+                            position: toast.POSITION.TOP_CENTER,
+                            theme: "colored"
+                        });
+                        break;
+                    default:
+                        toast.error('Unknown error. Please check and try again!', {
+                            position: toast.POSITION.TOP_CENTER,
+                            theme: "colored"
+                        });
+                        break;
+                }
+                return Promise.reject(error);
             }
-            return Promise.reject(error);
-        }
-    );
+        )
+    }, [setLoading])
     return children
 }
 export default WithAxios
